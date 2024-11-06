579d2cb50faf82eeea6fd0193552e8f344f6a28b
Sdunsmore2006 <sdunsmore244@gmail.com>
2024-11-06 09:37:25 -0500

    feat: added structure to tests

--- app/src/test/java/opp/project/t4/KanbanAppTest.java
+++ app/src/test/java/opp/project/t4/KanbanAppTest.java
@@ -6,9 +6,6 @@ package opp.project.t4;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
-class AppTest {
-    @Test void appHasAGreeting() {
-        App classUnderTest = new App();
-        assertNotNull(classUnderTest.getGreeting(), "app should have a greeting");
-    }
+class KanbanAppTest {
+    
 }

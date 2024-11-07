c5e409f3d11ede6f6a846fd5c49b46b5f5c3041f
“A1-4U2T1NN” <a1.austinn19@gmail.com>
2024-11-07 16:34:51 -0500

    feat: added possible methods to KanbanApp.java; updated User.java file and corresponding test with code; Created UserTaskManager.java file and corresponding test with code;

--- app/src/main/java/opp/project/t4/KanbanApp.java
+++ app/src/main/java/opp/project/t4/KanbanApp.java
@@ -1,4 +1,13 @@
 package opp.project.t4;
+
+import java.util.UUID;
+
 public class KanbanApp{
 
+    public void addUser(User user) { 
+    
+    }
+    public void removeUser(UUID userId) {
+
+    }
 }
